3cf282e6c4bca60308b9c28989490c8df7dd2b6c
Пуритан Шидаковец <nikita@mbp-nikita.fritz.box>
2018-05-25 14:56:14 +0200

    version 0.1

---
+++ Tester/New Group/Question.swift
@@ -0,0 +1,75 @@
+//
+//  Question.swift
+//  Einbürgerungstest
+//
+//  Created by Nikita Popov on 20.04.2018.
+//  Copyright © 2018 Nikita Popov. All rights reserved.
+//
+
+import Foundation
+struct Answer:Codable{
+    var text: String?
+    var id: Int?
+}
+struct Question: Codable{
+    
+    private(set) var textOfQuestion: String
+    private(set) var linkOfImageForQuestion: String
+    private(set) var answers: Array<Answer>
+    private(set) var idOfRightAnswer: Int?
+    private(set) var idOfQuestion: UInt32?
+    
+    init(withText text:String, withImage image: String, withAnswers: [String],withId id: UInt32 ){
+        textOfQuestion = text
+        linkOfImageForQuestion = image
+        idOfQuestion = id
+        answers = Array<Answer>()
+        for str in withAnswers {
+            answers.append(Answer(text: str, id: 16.arc4random))
+        }
+        idOfRightAnswer = answers.first?.id
+        answers.shuffle()
+    }
+    init(withText text:String, withImage image: String, withAnswers: [String]){
+        textOfQuestion = text
+        linkOfImageForQuestion = image
+        idOfQuestion = UInt32(100000.arc4random)
+        answers = Array<Answer>()
+        for str in withAnswers {
+            answers.append(Answer(text: str, id: 16.arc4random))
+        }
+        idOfRightAnswer = answers.first?.id
+        answers.shuffle()
+    }
+}
+
+extension Int{
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
+//{
+//    "textOfQuestion": "String",
+//    "linkOfImageForQuestion": "String",
+//    "textOfQuestion": "Array<Answer>",
+//    "textOfQuestion": "Int",
+//}
+extension MutableCollection {
+    mutating func shuffle(){
+        let c = count
+        guard c > 1 else {
+            return
+        }
+        for (firstUnshuffled, unshuffledCount) in zip(indices,stride(from: c, to: 1, by: -1)) {
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let i = index(firstUnshuffled, offsetBy: d)
+            swapAt(firstUnshuffled, i)
+        }
+    }
+}
--- Tester/ViewController.swift
+++ Tester/ViewController.swift
@@ -9,21 +9,107 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var test = Test(withQuestions: Data.init().hamburg)
+    var defaultColor = #colorLiteral(red: 0.8274509804, green: 0.8431372549, blue: 0.831372549, alpha: 1)
+    var trueSelectedColor = #colorLiteral(red: 0.8459352261, green: 1, blue: 0.662603968, alpha: 1)
+    var falseSelectedColor = #colorLiteral(red: 1, green: 0.7718817678, blue: 0.6401711963, alpha: 1)
+    var isAnswerSelected = false
+    var idOfSelectedAnswer = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showNewQuestion()
+        setupView()
+        var b = UIImage(named: "hamburg.frage1.png")
+        imageView.image = UIImage(named: "hamburg.frage1.png")
     }
     
+    @IBOutlet var answerButtons: [UIButton]!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    public var textOfQuestion: String? {
+        get {
+            return questionLabel.text
+        }
+        set {
+            questionLabel.text = newValue ?? nil
+        }
+    }
+    public var linkOfImage: String {
+        get {
+            return "sorry"
+        }
+        set {
+            if newValue != "" {
+                var image = UIImage(named: newValue)
+                imageView.image = image
+            }
+        }
+    }
+    @IBAction func onAnswerTouch(_ sender: UIButton) {
+        if !isAnswerSelected {
+        sender.backgroundColor = sender.tag == test.currentQuestion?.idOfRightAnswer ? trueSelectedColor : falseSelectedColor
+            idOfSelectedAnswer = sender.tag
+            isAnswerSelected = true
+        }
+    }
     
-    @IBAction func onButtonClick(_ sender: UIButton!) {
-        print("view did load")
+    @IBAction func onNextTouch(_ sender: UIButton) {
+        test.nextAnswer(idOfAnswer: idOfSelectedAnswer)
+        isAnswerSelected = false
+        showNewQuestion()
+        updateView()
+        
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func showNewQuestion(){
+        if let question = test.nextQuestion(){
+            linkOfImage = question.linkOfImageForQuestion
+            textOfQuestion = question.textOfQuestion
+            for n in question.answers.indices {
+                let answer = question.answers[n]
+                answerButtons[n].setTitle(answer.text, for: UIControlState.normal)
+                answerButtons[n].tag = answer.id!
+            }
+        } else {
+            //clearView()
+            textOfQuestion = "Sie haben \(test.rightAnswers) von \(test.size) richtig beantwortet"
+        }
     }
-
+    func setupView() {
+        
+        for but in answerButtons {
+            but.layer.cornerRadius = 5
+            but.clipsToBounds = true
+            but.backgroundColor = defaultColor
+        }
+        nextButton.backgroundColor = defaultColor
+        nextButton.layer.cornerRadius = 5
+        nextButton.clipsToBounds = true
+        questionLabel.backgroundColor = defaultColor
+        questionLabel.layer.cornerRadius = 5
+        questionLabel.clipsToBounds = true
+    }
+    func updateView() {
+        for but in answerButtons {
+            but.backgroundColor = defaultColor
+        }
+    }
 
 }
 
+struct Data{
+    let hamburg = [
+        Question(withText: "Welches Wappen gehört zur Freien und Hansestadt Hamburg?", withImage: "hamburg.frage1.png", withAnswers: ["2","1","3","4"]),
+        Question(withText: "Welches ist ein Bezirk von Hamburg? ", withImage: "", withAnswers: ["Altona","Hemelingen","Pankow","Mecklenburgische Seenplatte"]),
+        Question(withText: "Für wie viele Jahre wird das Landesparlament in Hamburg gewählt?", withImage: "", withAnswers: ["5","4","3","6"]),
+        Question(withText: "Ab welchem Alter darf man in Hamburg bei Kommunalwahlen (Wahl der Bezirksversammlungen) wählen?", withImage: "", withAnswers: ["16", "14","18","20"]),
+        Question(withText: "Welche Farben hat die Landesflagge von Hamburg?", withImage: "", withAnswers: ["weiß-rot","blau-weiß-rot","grün-weiß-rot","schwarz-gelb"]),
+        Question(withText: "Wo können Sie sich in Hamburg über politische Themen informieren?", withImage: "", withAnswers: ["bei der Landeszentrale für politische Bildung", "beim Ordnungsamt der Gemeinde","bei der Verbraucherzentrale"," bei den Kirchen"]),
+        Question(withText: "Welches Bundesland ist ein Stadtstaat? ", withImage: "", withAnswers: ["Hamburg", "Sachsen","Bayern","Thüringen"]),
+        Question(withText: "Welches Bundesland ist Hamburg?", withImage: "hamburg.frage8.png", withAnswers: ["3", "1","2","4"]),
+        Question(withText: "Wie nennt man den Regierungschef / die Regierungschefin des Stadtstaates Hamburg?", withImage: "", withAnswers: ["Erster Bürgermeister / Erste Bürgermeisterin", "Ministerpräsident / Ministerpräsidentin","Regierender Senator / Regierende Senatorin"," Oberbürgermeister / Oberbürgermeisterin"]),
+        Question(withText: "Welchen Senator / welche Senatorin hat Hamburg nicht?", withImage: "", withAnswers: ["Senator / Senatorin für Außenbeziehungen", "Justizsenator / Justizsenatorin","Finanzsenator / Finanzsenatorin","Innensenator / Innensenatorin"]),
+        ]
+    
+}
